976e706d02b06099b6c923da2d9f9d358fff310b
Ayden Best <abest717@gmail.com>
2019-10-11 21:51:25 +0000

    Added GenStack module

---
+++ GenStack/GenStack.h
@@ -0,0 +1,37 @@
+// Ayden Best
+// 2317839
+
+#include <iostream>
+#include "GenStackEmptyException.h"
+
+using namespace std;
+
+template <typename T>
+class GenStack {
+private:
+  int maxSize;                // Maximum number of elements allowed in stackArray
+  int t;                    // The index of the top element in stackArray
+  T* stackArray;             // The array that holds all items in the stack
+
+  void grow();              // Doubles the size of the stack, in case the stack is full and needs more room
+
+public:
+
+    GenStack();               // Default Constructor
+    GenStack(int startSize);  // Overloaded Constructor
+    ~GenStack();                // Destructor
+
+    T pop();                  // Removes and returns the top element
+
+    T top();                  // Returns the top element
+
+    void push(T element);     // Pushes an element to the top of the stack
+
+    int size();               // Returns the number of items in the stack
+
+    bool empty();             // Returns true if the stack is empty
+
+
+};
+
+#include "GenStackImplementation.h"
---
+++ GenStack/GenStackImplementation.h
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+// Default Constructor
+template <typename T>
+GenStack<T>::GenStack()
+{
+  maxSize = 1;
+  t = -1;
+  stackArray = new T[1];
+}
+
+// Overloaded Constructor
+template <typename T>
+GenStack<T>::GenStack(int startSize)
+{
+  maxSize = startSize;
+  t = -1;
+  stackArray = new T[startSize];
+}
+
+// Destructor
+template <typename T>
+GenStack<T>::~GenStack()
+{
+  delete[] stackArray;
+  stackArray = NULL;
+}
+
+// Removes and returns the top element
+template <typename T>
+T GenStack<T>::pop()
+{
+  if (empty())
+    throw GenStackEmptyException();
+  --t;
+  return stackArray[t+1];
+}
+
+// Returns the top element
+template <typename T>
+T GenStack<T>::top()
+{
+  if (empty())
+    throw GenStackEmptyException();
+  return stackArray[t];
+}
+
+// Pushes an element to the top of the stack
+template <typename T>
+void GenStack<T>::push(T element)
+{
+  if (t+1 == maxSize)
+    grow();
+  ++t;
+  stackArray[t] = element;
+}
+
+// Returns the number of items in the stack
+template <typename T>
+int GenStack<T>::size()
+{
+  return (t+1);
+}
+
+// Returns true if the stack is empty
+template <typename T>
+bool GenStack<T>::empty()
+{
+  return (t = -1);
+}
+
+// Doubles the size of the stack, in case the stack is full and needs more room
+template <typename T>
+void GenStack<T>::grow()
+{
+  T *temp = new T[maxSize*2]; // new array twice the size
+  for (int i=0 ; i<maxSize ; ++i){ // fill new array with old information
+    temp[i] = stackArray[i];
+  }
+  delete[] stackArray; // clean up
+  stackArray = temp;
+  temp = NULL;
+}
