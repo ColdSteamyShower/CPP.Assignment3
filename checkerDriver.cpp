cf8779029497b8775bd1ac9aa70e7ff64645ed57
Ayden <abest717@gmail.com>
2019-10-15 23:48:42 +0000

    Allowed driver to prompt for new file

--- checkerDriver.cpp
+++ checkerDriver.cpp
@@ -6,14 +6,30 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-  string file = argv[1];                                                        //
-  ifstream inFile(file);                                                        // Make sure that the file exists
-  if (!inFile)                                                                  //
-  {                                                                             //
-    cout << "File '" << file << "' cannot be read or does not exist" << endl;   //
-  } else {
-    inFile.close();             //
-    syntaxChecker checker;      // if it does, we're allowed to run the program
-    checker.checkFile(file);    //
+
+  //initialize
+  string file = argv[1];
+  syntaxChecker checker;
+
+  while (true)
+  {
+    ifstream inFile(file);
+    if (!inFile)
+    { // If the file doesn't exist, notify the user
+      cout << "File '" << file << "' cannot be read or does not exist" << endl;
+    }
+    else
+    { // If the file exists, check the file
+      inFile.close();
+      if (checker.checkFile(file)){ // if there were no errors, continue
+        cout << "No Errors were found" << endl;
+      } else { // if there were errors, quit
+        break;
+      }
+    }
+    cout << "Enter a file name or enter 'e' to exit the program" << endl;
+    cin >> file;
+    if (file == "e")
+      break;
   }
 }
